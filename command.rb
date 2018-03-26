8ac0cf4abbec23b94bc0e0572e1390e2c0f458de
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-03-27 02:10:00 +0900

    use prezto

--- command.rb
+++ command.rb
@@ -1,34 +1,12 @@
 # node[:user] = node[:user] || 'root'
 
-execute "add fish in /etc/shells" do
-  command "echo /usr/local/bin/fish >> /etc/shells"
-  not_if 'cat /etc/shells | grep "/usr/local/bin/fish"'
-end
-
-# install fisherman
-execute "install fisherman" do
-  command "curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher"
-end
-
-execute "wget config.fish" do
-  command "wget https://raw.githubusercontent.com/mitubaEX/dotfiles/master/config.fish -P ~/.config/fish/."
-  not_if 'test -e ~/.config/fish/config.fish'
-end
-
 execute "wget .zshrc" do
   command "wget https://raw.githubusercontent.com/mitubaEX/zsh_list/master/.zshrc -P ~/."
   not_if 'test -e ~/.zshrc'
 end
-execute "wget .zshenv" do
-  command "wget https://raw.githubusercontent.com/mitubaEX/zsh_list/master/.zshenv -P ~/."
-  not_if 'test -e ~/.zshenv'
-end
-execute "wget .zsh_history" do
-  command "wget https://raw.githubusercontent.com/mitubaEX/zsh_list/master/.zsh_history -P ~/."
-  not_if 'test -e ~/.zsh_history'
+
+execute "wget .zpreztorc" do
+  command "wget https://raw.githubusercontent.com/mitubaEX/zsh_list/master/.zpreztorc -P ~/."
+  not_if 'test -e ~/.preztorc'
 end
 
-# execute "change bash to fish" do
-#   command "chsh -s /usr/local/bin/fish #{node[:user]}"
-#   not_if 'test $SHELL = "/usr/local/bin/fish"'
-# end
--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ itamae local package.rb
 itamae local command.rb vim_settings.rb
 export PYTHONPATH=python3:pip3
 pip3 install neovim
-nvim -e -c UpdateRemotePlugins
+nvim -e -c "call dein#install()"
+nvim -e -c "UpdateRemotePlugins"
