44ba01e19e1cbd65ada7e1575233ebf7c2a6b703
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-04-09 14:23:50 +0900

    add git config

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ export PYTHONPATH=python3:pip3
 pip3 install neovim
 nvim -e -c "call dein#install()"
 nvim -e -c "UpdateRemotePlugins"
+
+# git
+git config --global core.editor 'nvim -c "set fenc=utf-8"'
+git config --global user.name "mitubaEX"
+git config --global user.email "g1344955@cse.kyoto-su.ac.jp"
