059e09ccd83227cc2a734eb9e1c2f510ce487d96
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-05-04 14:48:10 +0900

    change zsh_list to dotfiles

--- command.rb
+++ command.rb
@@ -1,12 +1,12 @@
 # node[:user] = node[:user] || 'root'
 
 execute "wget .zshrc" do
-  command "wget https://raw.githubusercontent.com/mitubaEX/zsh_list/master/.zshrc -P ~/."
+  command "wget https://raw.githubusercontent.com/mitubaEX/dotfiles/master/.zshrc -P ~/."
   not_if 'test -e ~/.zshrc'
 end
 
 execute "wget .zpreztorc" do
-  command "wget https://raw.githubusercontent.com/mitubaEX/zsh_list/master/.zpreztorc -P ~/."
+  command "wget https://raw.githubusercontent.com/mitubaEX/dotfiles/master/.zpreztorc -P ~/."
   not_if 'test -e ~/.preztorc'
 end
 
