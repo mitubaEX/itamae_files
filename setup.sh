2fb495fa03beb18979a879c0e80e85c9f36159d5
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-09-20 08:17:16 +0000

    add vim setting to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 itamae local package.rb
-itamae local command.rb vim_settings.rb
+# itamae local command.rb vim_settings.rb
+itamae local command.rb
 export PYTHONPATH=python3:pip3
 pip3 install neovim
 nvim -e -c "call dein#install()"
@@ -39,3 +40,7 @@ go get -u github.com/mdempsky/gocode
 
 # haskell
 stack setup
+
+# vim
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ./installer.sh
+sh ./installer.sh ~/.cache/dein/. ; rm ./installer.sh
