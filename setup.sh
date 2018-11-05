3fce33bad3a7979bfe078221569dad1478a9e352
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-11-05 22:01:24 +0900

    change core.editor nvim -> nvr

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ nvim -e -c "call dein#install()"
 nvim -e -c "UpdateRemotePlugins"
 
 # git
-git config --global core.editor 'nvim -c "set fenc=utf-8"'
+git config --global core.editor 'nvr'
 git config --global user.name "mitubaEX"
 git config --global user.email "g1344955@cse.kyoto-su.ac.jp"
 
